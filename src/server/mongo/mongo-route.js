b57d96ad6b51aa1fcb9eb9ae773d49b364dfca93
David Souther <davidsouther@gmail.com>
2015-05-21 10:18:22 -0400

    Secure the mongo routes

--- src/server/mongo/mongo-route.js
+++ src/server/mongo/mongo-route.js
@@ -20,6 +20,16 @@ module.exports = function(server, config){
 
   var mongo_root_path = config.find('mongo.rest.path', '/api');
 
+
+  server.all(mongo_root_path + '/*', function(request, response, next){
+    if(request.cookies.session === '1234'){ // Look up in session DB
+      next();
+    } else {
+      response.status(403).send({notLoggedIn: 1});
+    }
+  });
+
+
   var connection = {
     username: config.find('mongo.username', 'MONGO_USERNAME', ''),
     password: config.find('mongo.username', 'MONGO_PASSWORD', ''),
