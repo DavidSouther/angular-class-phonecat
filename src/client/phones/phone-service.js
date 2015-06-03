7c2798fb25bf0cff1e3ef3cf761ea58e5c3f80ba
David Souther <davidsouther@gmail.com>
2015-06-03 09:02:50 -0500

    Added migration script.

--- src/client/phones/phone-service.js
+++ src/client/phones/phone-service.js
@@ -1,10 +1,39 @@
-PhoneService.$inject = ['$resource'];
-function PhoneService($resource){
-  return $resource('/api/phonecat/phones/:phoneId');
+PhoneService.$inject = ['$resource', '$ajaxLog'];
+function PhoneService($resource, $log, $screenshots){
+  return $resource('/api/phonecat/phones/:phoneId', {
+    phoneId: '@id'
+  });
+}
+
+function StaticPhoneService($resource){
+  return $resource('/phones/:phoneId.json', [], {
+    query: {
+      method: 'GET',
+      params: {
+        phoneId: 'phones'
+      },
+      isArray: true
+    }
+  });
 }
 
 angular.module('phonecat.phones.service', [
   'ngResource'
 ])
 .factory('Phone', PhoneService)
+.factory('StaticPhone', StaticPhoneService)
+.run(function($location, Phone, StaticPhone){
+  if(!$location.search().migrate){
+    return;
+  }
+  StaticPhone.query(function(phones){
+    phones.forEach(function(data){
+      StaticPhone.get({phoneId: data.id}, function(phone){
+        (new Phone(data)).$save(function(){
+          console.log("Saved phone %o", phone);
+        });
+      })
+    });
+  });
+});
 ;
